5eda6ccb86cd03ca76c617a4e37244143a2e834d
Natalie Kozlowski <kozlow47@gmail.com>
2017-03-12 11:31:16 -0400

    NK add filters on cat pages

--- spartan_life/archive.php
+++ spartan_life/archive.php
@@ -9,41 +9,60 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <section id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+        <?php
+        if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					get_search_form();
-				?>
-			</header><!-- .page-header -->
+            <header class="page-header">
+                <div class="contain_cat_search">
+                    <?php
+                        get_search_form();
+                    ?>
+                    <?php if (is_category( 'Recipes' )) { ?>
+                        <ul class="filter_archive_tags">
+                            <li>Filter By:</li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/breakfast">Breakfast</a></li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/entree">Entrée</a></li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/dessert">Dessert</a></li>
+                            <!-- <li><a href="<?php #echo esc_url( home_url( '/' ) ); ?>/tag/snack">Snack</a></li> -->
+                        </ul>
+                    <?php } elseif (is_category( 'Travel' )) { ?>
+                        <ul class="filter_archive_tags">
+                            <li>Filter By:</li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/north-america">North America</a></li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/europe">Europe</a></li>
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/tag/asia">Asia</a></li>
+                        </ul>
+                    <?php } else { ?>
+                    <?php } ?>
+                </div>
+            </header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+            <?php
+            /* Start the Loop */
+            while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+                /*
+                 * Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                get_template_part( 'template-parts/content', 'search' );
 
-			endwhile;
+            endwhile;
 
-			wpbeginner_numeric_posts_nav();
+            wpbeginner_numeric_posts_nav();
 
-		else :
+        else :
 
-			get_template_part( 'template-parts/content', 'search' );
+            get_template_part( 'template-parts/content', 'search' );
 
-		endif; ?>
+        endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+        </main><!-- #main -->
+    </section><!-- #primary -->
 
 <?php
 get_footer();
